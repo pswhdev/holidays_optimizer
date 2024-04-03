643df1abe0cc642d42f21c7f010a83436ffd075b
pswhdev <patriciaswhalley@gmail.com>
2024-04-03 11:02:30 +0200

    Improve coments on code

--- run.py
+++ run.py
@@ -59,19 +59,20 @@ def get_country():
             # Converts input to lowercase and deletes empty spaces
             user_input = user_input.strip().lower()
 
-            # Validation to check if input is a number instead of text
+            # Validation block to check if input is not empty, space or digit instead of text.
             if user_input.isdigit():
                 raise ValueError(
-                    "Please enter a valid country " "name (text, not a number)."
+                    "Please enter a valid country name (text, not a number)."
                 )
-
-            # Validation to prevent submission of empty input or white spaces
             elif not user_input:
-                raise ValueError("Input cannot be empty. Please enter a country.")
-
-            # To find the country's abbreviation given the country name
+                raise ValueError(
+                    "Input cannot be empty. Please enter a country."
+                )
+            # If the input is valid:
+            # To find the country's abbreviation given the country name to work with the holidays library
             for country, abbreviation in database.countries.items():
                 if user_input == country.lower():
+                    # Confirmation block with the user about county's choice, it keeps on asking for a valid answer ('y' or 'n')
                     while True:
                         print(
                             f"[bright_green]The selected country was {country}. [/bright_green]"
@@ -82,17 +83,22 @@ def get_country():
                             .lower()
                         )
                         if confirmation == "y":
-                            # To return the country's abbreviation used on the holidays library
+                            # If the user confirms, it returns the country's abbreviation (used on the holidays library)
                             return abbreviation
                         elif confirmation == "n":
-                            # Prompt for another country
+                            # If the user decides to choose another country it has to get out of this inner loop
                             break
                         else:
                             print("Please enter 'y' or 'n'.")
-                    # Prompt for another country
+                    # Since the first input (country's name) was a valid one,
+                    # if the user's input was 'n' inside the confirmation block,
+                    # the inner loop is stopped and the outer loop also needs to be stopped
+                    # so the block restarts and prompts for choice of country again.
                     break
             else:
-                raise ValueError("Please enter a valid country name in English.")
+                raise ValueError(
+                    "Please enter a valid country name in English."
+                )
         except ValueError as e:
             print("[bright_red]Invalid input.[/bright_red]", e)
 
@@ -116,7 +122,9 @@ def specify_state(country):
                         f"[bright_green]The selected state/territory/province was {state_input}. [/bright_green]"
                     )
                     confirmation = (
-                        input(f"Is this the desired one? (y/n): ").strip().lower()
+                        input(f"Is this the desired one? (y/n): ")
+                        .strip()
+                        .lower()
                     )
                     if confirmation == "y":
                         return state_input
@@ -156,7 +164,9 @@ def get_date(message):
             selected_date = datetime.strptime(selected_date_str, "%d-%m-%Y")
 
             if selected_date < datetime.now():
-                print("[bright_red]You cannot choose a date in the past. [/bright_red]")
+                print(
+                    "[bright_red]You cannot choose a date in the past. [/bright_red]"
+                )
                 raise ValueError(
                     f"Today is {datetime.now().strftime('%d-%m-%Y')}. Please choose a date in the future."
                 )
@@ -193,10 +203,14 @@ def verify_dates(start_date, end_date):
             if end_date < start_date:
                 raise ValueError("End date cannot be before the start date.")
             if end_date == start_date:
-                raise ValueError("End date cannot be the same as the start date.")
+                raise ValueError(
+                    "End date cannot be the same as the start date."
+                )
             # Check if dates are maximum one year apart (https://docs.python.org/3/library/datetime.html#timedelta-objects)
             if (end_date - start_date).days > 366:
-                raise ValueError("Please enter dates that are maximum one year apart.")
+                raise ValueError(
+                    "Please enter dates that are maximum one year apart."
+                )
             # To break the loop in case there is no problem with the chosen dates validation
             return start_date, end_date
         except ValueError as e:
@@ -212,9 +226,11 @@ def confirm_dates(start_date, end_date):
     """
     while True:
         print(
-            f"[bright_green]The selected period was [/bright_green]{start_date.strftime('%d-%m-%Y')} [bright_green] and [/bright_green]{end_date.strftime('%d-%m-%Y')}. "
+            f"[bright_green]The selected period was [/bright_green]{start_date.strftime('%d-%m-%Y')}[bright_green] and [/bright_green]{end_date.strftime('%d-%m-%Y')}[bright_green].[/bright_green]"
         )
-        confirmation = input("Are you happy with these dates? (y/n): ").strip().lower()
+        confirmation = (
+            input("Are you happy with these dates? (y/n): ").strip().lower()
+        )
 
         if confirmation == "n":
             # Asks for new start and end dates
@@ -286,17 +302,20 @@ def filter_weekday_holidays(holiday_dict):
     """
     weekday_holidays = []
     for date, holiday in sorted(holiday_dict.items()):
+        # Using datetime to find day of the week (https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes)
         weekday = date.strftime("%A")
         # Filter out weekends (Saturday and Sunday)
         if weekday not in ["Saturday", "Sunday"]:
-            weekday_holidays.append({"name": holiday, "date": date, "weekday": weekday})
+            weekday_holidays.append(
+                {"name": holiday, "date": date, "weekday": weekday}
+            )
     return weekday_holidays
 
 
 def get_bridge_days(holidays):
     """
-    Identifies potential bridge days around public holidays to optimize vacation days. 
-    Analyzes weekdays holidays to suggest days off for extended breaks. Considers taking 1, 2 and 
+    Identifies potential bridge days around public holidays to optimize vacation days.
+    Analyzes weekdays holidays to suggest days off for extended breaks. Considers taking 1, 2 and
     4 days vacation in special cases like consecutive Friday and Monday holidays for longer breaks.
     """
     four_days_1 = {}
@@ -334,7 +353,7 @@ def get_bridge_days(holidays):
                     (holiday["date"] + timedelta(days=7)),
                 ]
             # Logic for one and two days
-            elif following_monday_holiday == False:
+            elif not following_monday_holiday:
                 one_day[holiday["name"]] = [
                     (holiday["date"] + timedelta(days=-1)),
                     (holiday["date"] + timedelta(days=3)),
@@ -402,7 +421,9 @@ def get_bridge_days(holidays):
             "\n[bright_green]By taking[/bright_green] 1 [bright_green]vacation day on the suggested date(s), you will have a long weekend of at least[/bright_green] 4 [bright_green]days.[/bright_green]"
         )
         for holiday_name, dates in one_day.items():
-            dates_str = " or ".join([date.strftime("%d-%m-%Y") for date in dates])
+            dates_str = " or ".join(
+                [date.strftime("%d-%m-%Y") for date in dates]
+            )
             print(f"{holiday_name}: Take {dates_str}")
 
     if two_days_1:
@@ -410,7 +431,9 @@ def get_bridge_days(holidays):
             "\n[bright_green]By taking [/bright_green]2 [bright_green]vacation days on the suggested dates, you will have an extended break of at least[/bright_green] 5 [bright_green]days.[/bright_green]"
         )
         for holiday_name, dates in two_days_1.items():
-            dates_str = " and ".join([date.strftime("%d-%m-%Y") for date in dates])
+            dates_str = " and ".join(
+                [date.strftime("%d-%m-%Y") for date in dates]
+            )
             print(f"{holiday_name}: Take {dates_str}")
 
     if two_days_2:
@@ -418,7 +441,9 @@ def get_bridge_days(holidays):
             "\n[bright_green]Alternatively, consider taking these [/bright_green]2 [bright_green]vacation days for an equally extended break of at least[/bright_green] 5 [bright_green]days.[/bright_green]"
         )
         for holiday_name, dates in two_days_2.items():
-            dates_str = " and ".join([date.strftime("%d-%m-%Y") for date in dates])
+            dates_str = " and ".join(
+                [date.strftime("%d-%m-%Y") for date in dates]
+            )
             print(f"{holiday_name}: Take {dates_str}")
 
     return four_days_1, four_days_2, one_day, two_days_1, two_days_2
@@ -447,8 +472,8 @@ def what_next():
 
 def main():
     """
-    Controls the order of execution of the functions guiding the user through the process of selecting a country, 
-    specifying dates, and checking for public holidays. It then suggests optimal days for taking time off to maximize 
+    Controls the order of execution of the functions guiding the user through the process of selecting a country,
+    specifying dates, and checking for public holidays. It then suggests optimal days for taking time off to maximize
     holiday periods.
     """
     prints_logo()
